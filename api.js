12b3353519b5fe6849723d3a52603df420c1fcda
MatkovskyA <a.matkovsky14@gmail.com>
2023-12-12 18:36:06 +0300

    вывел список фильмов по запросу из инпута

--- api.js
+++
@@ -1,7 +0,0 @@
-const apiKey = '7e07b9f6';
-
-
-function getMovies(searchQuery) {
-    return fetch(`https://www.omdbapi.com/?s=${searchQuery}&apiKey=[${apiKey}]`)
-    .then(data => data.json())
-}
--- movies.js
+++ movies.js
@@ -1,18 +1,199 @@
-const searchInputNode = document.querySelector('#search');
-const searchBtnNode = document.querySelector('#search-btn');
+//получаем элементы HTML
+const btnSearchNode = document.querySelector(".button");
+let inputNode = document.querySelector('.input-text');
+const messageErrorNode = document.querySelector('.search-error');
+const ulList = document.querySelector('.list');
 
-searchBtnNode.addEventListener('click', () => {
-    const searchQuery = searchInputNode.value;
 
-    if (!searchQuery) {
-        return;
+btnSearchNode.addEventListener('click', searchMovie)
+
+
+
+async function findMovies(movieTitle) {
+    const res = await fetch(`https://www.omdbapi.com/?s=${movieTitle}&apikey=7e07b9f6`);
+    const data = await res.json();
+    if(data.Response === "False") {
+        messageErrorNode.classList.remove('hidden')
+    } else {
+        showMovies(data.Search)
     }
     
-    getMovies(searchQuery) 
-        .then(result => {
-            const movies = result.Search;
+}
+// findMovies()
 
-            console.log(movies);
-        });
+//получаем вводимые данные из инпута и отправляем его в поиск на сервер
+function searchMovie() { 
+    let movieTitle = inputNode.value.trim();
+    if(!movieTitle) {
+        return messageErrorNode.classList.remove('hidden')
+    } else {
+        messageErrorNode.classList.add('hidden')
+    }
+    findMovies(movieTitle);
+    ulList.classList.remove('hidden')
+    
+}
 
-})
+
+//отрисовываем список полученных фильмов
+function showMovies(movies){
+    let moviesList = '';
+
+    movies.map(movie => {
+        let movieImg = 'resourses/block.jpeg';
+        if(movie.Poster !== "N/A;") {
+            movieImg = movie.Poster
+        }
+        moviesList += `
+        <li>
+            <div>
+            <img src = ${movie.Poster};
+            </div>
+            <div>
+            <h1>${movie.Title}</h1>
+            <p>${movie.Type}</p>
+            <p>${movie.Year}</p>
+            </div>
+        </li>
+        `
+    })
+    ulList.innerHTML = moviesList;
+}
+
+
+
+
+
+
+// константы из HTML
+// const inputNode = document.getElementById("search__input");
+// const searchList = document.getElementById("search__list");
+// const resultNode = document.getElementById("result__movie-list");
+// const adviceNode = document.getElementById("advice");
+
+
+// ищем в базе API фильм по названию
+// async function loadMovies(movieTitle) {
+//     const url = `https://www.omdbapi.com/?s=${movieTitle}&page=1&apikey=57e731e0`;
+//     const res = await fetch(`${url}`);
+//     const data = await res.json();
+//     // проверка, что ответ получен
+//     if (data.Response === "True") {
+//       showMovies(data.Search);
+//       adviceNode.classList.add("advice__hide");
+//     } else {
+//       searchList.classList.add("search__list-hide");
+//       adviceNode.classList.remove("advice__hide");
+//     }
+//   }
+  //--------------------------------------------------------------------
+//   // поиск и скрытие списка
+//   function findMovies() {
+//     let movieTitle = inputNode.value.trim();
+//     // если введен хотя бы 1 знак(кроме только пробелов) в инпут
+//     // будет показан список фильмов на основе введенных знаков
+//     if (movieTitle.length > 0) {
+//       searchList.classList.remove("search__list-hide");
+//       loadMovies(movieTitle);
+//     } else {
+//       // если пусто в инпуте, то список скроется
+//       searchList.classList.add("search__list-hide");
+//       adviceNode.classList.add("advice__hide");
+//     }
+//   }
+    //--------------------------------------------------------------------
+//   // отображаем фильмы, подходящие по названию
+//   function showMovies(movies) {
+//     searchList.innerHTML = "";
+//     // coздаем блок со списком фильмов
+//     for (i = 0; i < movies.length; i++) {
+//       let searchListMovie = document.createElement("div");
+//       // даём каждому элементу внутри списка(фильму)
+//       // id соответствующий id с API
+//       searchListMovie.dataset.id = movies[i].imdbID;
+//       searchListMovie.classList.add("search__list-movie");
+//       // проверяем на наличие постера у фильма
+//       if (movies[i].Poster !== "N/A") {
+//         // если есть, то подгружаем постер с API
+//         moviePoster = movies[i].Poster;
+//       } else {
+//         // если нет, то подгружаем нашу картинку
+//         moviePoster = "resources/no-img.png";
+//       }
+//       // записываем HTML код для каждого элемента списка
+//       searchListMovie.innerHTML = `
+//         <div class="search__movie-thumbnail">
+//           <img src="${moviePoster}"/>
+//         </div>
+//         <div class="search__movie-info">
+//           <h3>${movies[i].Title}</h3>
+//           <p>${movies[i].Year}</p>
+//         </div>
+//       `;
+//       // добавляем элемент в наш выпадающий список
+//       searchList.appendChild(searchListMovie);
+//     }
+//     loadMovieInfo();
+//   }
+  
+//   // получаем всю информацию о фильме по id при клике
+//   function loadMovieInfo() {
+//     const searchListMovies = searchList.querySelectorAll(".search__list-movie");
+//     // дальше для каждого элемента из коллекции searchListMovies:
+//     searchListMovies.forEach((movie) => {
+//       movie.addEventListener("click", async () => {
+//         searchList.classList.add("search__list-hide");
+//         const url = `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=57e731e0`;
+//         const res = await fetch(`${url}`);
+//         const movieInfo = await res.json();
+//         showMovieInfo(movieInfo);
+//       });
+//     });
+//   }
+  
+//   // показываем всю информацию о выбранном фильме и
+//   // создаем разметку в списке результатов после выбора
+//   function showMovieInfo(movie) {
+//     resultNode.innerHTML = `
+//       <div class="result__movie-img">
+//         <img src="${
+//           movie.Poster !== "N/A" ? movie.Poster : "resources/no-img.png"
+//         }" alt="movie-poster" />
+//       </div>
+//       <div class="result__movie-info">
+//         <h3 class="movie__title">${movie.Title}</h3>
+//         <ul class="movie__info-list">
+//           <li class="year">Year: ${movie.Year}</li>
+//           <li class="rated">Ratings: ${movie.Rated}</li>
+//           <li class="released">Released: ${movie.Released}</li>
+//         </ul>
+//         <p class="genre">Genre: ${movie.Genre}</p>
+//         <p class="writer">
+//           Writer: ${movie.Writer}
+//         </p>
+//         <p class="actors">
+//           Actors: ${movie.Actors}
+//         </p>
+//         <p class="plot">
+//           Plot: ${movie.Plot}
+//         </p>
+//         <p class="language">Language: ${movie.Language}</p>
+//         <p class="awards">Awards: ${movie.Awards}</p>
+//       </div>
+//     `;
+//   }
+  
+//   // cлушатели событий---------------------------------------------
+  
+//   // запуск функции, когда символ нажат и отпущен
+//   inputNode.addEventListener("keyup", findMovies);
+  
+//   inputNode.addEventListener("click", findMovies);
+  
+//   // скрывает выпадающий список по нажатию где-либо вне инпута
+//   window.addEventListener("click", (e) => {
+//     if (e.target.className !== "search__input") {
+//       searchList.classList.add("search__list-hide");
+//     }
+//   });
+  
